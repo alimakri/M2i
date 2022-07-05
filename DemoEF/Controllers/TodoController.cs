b16bd8ad64f0fb9bd02d52cc9f9f96c512e9b03f
Ali MAKRI <github@makri.fr>
2022-07-05 15:03:20 +0200

    15:03

---
+++ DemoEF/Controllers/TodoController.cs
@@ -0,0 +1,70 @@
+﻿using DemoEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoEF.Controllers
+{
+    public class TodoController : Controller
+    {
+        public DataPage1 Data = new DataPage1();
+
+        public ActionResult Index()
+        {
+            return View(Data);
+        }
+
+        // GET: Todo/Details/5
+        public ActionResult Details(int id)
+        {
+            var todo = Data.Todos.Where(t => t.Id == id).FirstOrDefault();
+            return View(todo);
+        }
+
+        // GET: Todo/Create
+        public ActionResult Create()
+        {
+            return View(new Todo());
+        }
+
+        // POST: Todo/Create
+        [HttpPost]
+        public ActionResult Create(Todo todoCree)
+        {
+            Data.Add(todoCree);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Todo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var todo = Data.Todos.Where(t => t.Id == id).FirstOrDefault();
+            return View(todo);
+        }
+
+        // POST: Todo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Todo todoEdit)
+        {
+            Data.Edit(todoEdit);
+            return View(todoEdit);
+        }
+
+        // GET: Todo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var todo = Data.Todos.Where(t => t.Id == id).FirstOrDefault();
+            return View(todo);
+        }
+
+        // POST: Todo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Todo todoDelete)
+        {
+            Data.Delete(todoDelete);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DemoEF/Models/Todo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoEF.Models
+{
+    public class DataPage1
+    {
+        public TodoContext Context;
+
+        public DataPage1()
+        {
+            Titre = "Ma première page ASP.Net MVC 5";
+            Context = new TodoContext();
+            Todos = Context.Todoes.ToList();
+        }
+
+        internal void Add(Todo todoCree)
+        {
+            Context.Todoes.Add(todoCree);
+            Context.SaveChanges();
+        }
+
+        internal void Edit(Todo todoEdit)
+        {
+            var fait = todoEdit.Fait ? "1" : "0";
+            var todo = Todos.Where(x => x.Id == todoEdit.Id).FirstOrDefault();
+            if (todo != null)
+            {
+                todo.Libelle = todoEdit.Libelle;
+                todo.Fait = todoEdit.Fait;
+                todo.DateExecution = todoEdit.DateExecution;
+                Context.SaveChanges();
+            }
+        }
+
+        public string Titre { get; set; }
+        public List<Todo> Todos { get; set; }
+
+        internal void Delete(Todo todoDelete)
+        {
+            var todo = Todos.Where(x => x.Id == todoDelete.Id).FirstOrDefault();
+            if (todo != null)
+            {
+                Context.Todoes.Remove(todo);
+                Context.SaveChanges();
+            }
+        }
+    }
+}
