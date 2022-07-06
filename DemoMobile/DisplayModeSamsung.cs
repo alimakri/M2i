4811b1daa2e8977088a08f794af106a4f6b8f0b6
Ali MAKRI <github@makri.fr>
2022-07-06 16:50:08 +0200

    16:50

---
+++ DemoMobile/DisplayModeSamsung.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace DemoMobile
+{
+    public class DisplayModeSamsung
+    {
+        public DisplayModeSamsung()
+        {
+            DisplayModeProvider.Instance.Modes.Insert(0,
+                new DefaultDisplayMode("samsung")
+                {
+                    ContextCondition = (
+                                    x => x.Request
+                                            .UserAgent
+                                            .IndexOf("samsung", StringComparison.OrdinalIgnoreCase) > 0
+                                    )
+                }
+            );
+        }
+    }
+}
