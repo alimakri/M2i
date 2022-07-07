08fca63fb8e4ffa2a6f8207aa3ec1c9c9f65e75c
Ali MAKRI <github@makri.fr>
2022-07-07 10:17:04 +0200

    10:16

--- DemoJs/Controllers/HomeController.cs
+++ DemoJs/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,18 @@ namespace DemoJs.Controllers
 {
     public class HomeController : Controller
     {
+        private Random Alea = new Random();
+        public string Couleur()
+        {
+            var couleur = new string[] { "Noir", "Black" };
+            switch (Alea.Next(1, 4))
+            {
+                case 1: couleur = new string[] { "Rouge", "Red" }; break;
+                case 2: couleur = new string[] { "Vert", "Green" }; break;
+                case 3: couleur = new string[] { "Bleu", "Blue" }; break;
+            }
+            return JsonConvert.SerializeObject(couleur);
+        }
         public ActionResult Index()
         {
             return View();
